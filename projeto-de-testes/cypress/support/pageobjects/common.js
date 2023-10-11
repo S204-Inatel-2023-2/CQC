b9c3d8b299dea1730de3505429eb1ca6cfdcda3a
dioic3 <joyce.dias@gec.inatel.br>
2023-10-11 11:14:10 -0300

    adição dos cenários de cadastro

--- projeto-de-testes/cypress/support/pageobjects/common.js
+++ projeto-de-testes/cypress/support/pageobjects/common.js
@@ -1,12 +1,32 @@
-import elements from "../elements/common";
-const UI = new elements;
-class common {
-    visit(){
-        cy.visit("https://davisbalbino.github.io/Cadastro/")
+import Elements from "../elements/common";
+const Common = new Elements;
+class PageObjetcs {
+    visit = () => {
+        cy.visit('https://davisbalbino.github.io/Cadastro/')
     }
 
-    validarPagina(){
-        cy.get(UI.titulo()).contains("Criar uma conta no Projeto")
+    validarMensagem = (id, legenda) => {
+        cy.get(Common.map(id)).contains(legenda)
+    }
+    
+    putValue = (value, placeholder) => {
+        if(placeholder == "Digite sua senha"){
+            cy.get(Common.map(placeholder)).first().type(value)
+        }
+        else if(placeholder == "Digite sua senha (Confirmação)"){
+            cy.get(Common.map(placeholder)).last().type(value)
+        }
+        else {
+            cy.get(Common.map(placeholder)).type(value)
+        }
+    } 
+
+    clickButton = (button, legend) => {
+        cy.get(Common.map(button)).contains(legend).click()
+    }
+
+    checkToast = (field, message) => {
+        cy.get(Common.map(field)).contains(message)
     }
 }
-export default common;
+export default PageObjetcs;
